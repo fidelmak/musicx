3c378febb64faf07740ffdf12667df90d3e6efc3
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-02-20 02:07:11 +0100

    Please enter the commit message for your changes. Lines starting

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,43 @@ class MusicX extends StatefulWidget {
   State<MusicX> createState() => _MusicXState();
 }
 
+// String song = song;
+// String artist = artist;
+
 class _MusicXState extends State<MusicX> {
   void playSound(int soundNumber) {
     final player = AudioPlayer();
-    player.play(AssetSource('mux$soundNumber.mp3'));
+    player.play(AssetSource('/muz/mux$soundNumber.mp3'));
   }
 
-  Expanded buildkey({
+  Card buildKey({
+    required Color color,
     required int soundNumber,
+    required String song,
+    required String artist,
   }) {
-    return Expanded(
-      child: Card(),
+    return Card(
+      color: color,
+      margin: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            leading: Image.asset('img/muxCard.jpg'),
+            title: Text(song, style: TextStyle(color: Colors.white)),
+            subtitle: Text(artist, style: TextStyle(color: Colors.white)),
+            trailing: TextButton(
+              onPressed: () {
+                // Add your onPressed code here
+              },
+              child: Icon(
+                Icons.play_circle_filled,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -53,22 +79,43 @@ class _MusicXState extends State<MusicX> {
             backgroundColor: Colors.grey,
             body: SafeArea(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   SizedBox(height: 10),
-                  Card(
-                    margin: const EdgeInsets.all(20),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        ListTile(
-                          leading: Image.asset('img/muxCard.jpg'),
-                          title: const Text('Demo Title'),
-                          subtitle:
-                              const Text('This is a simple card in Flutter.'),
-                        ),
-                      ],
+                  Container(
+                    height: 200,
+                    width: 300,
+                    child: Card(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          ListTile(
+                            leading: Image.asset(
+                              'img/ayra.png',
+                              fit: BoxFit.cover,
+                              height: 200.0,
+                              width: 200.0,
+                            ),
+                            title: const Text('Sound Mojo'),
+                            subtitle: const Text('play the boost out loud.'),
+                          ),
+                        ],
+                      ),
+                      elevation: 5,
+                      margin: const EdgeInsets.all(10),
                     ),
                   ),
+                  buildKey(
+                    color: Colors.green,
+                    soundNumber: 1,
+                    song: "Song title: buga",
+                    artist: "Artist name : Kiss Daniel",
+                  ),
+                  buildKey(
+                      color: Colors.black,
+                      soundNumber: 2,
+                      song: "Song title: yoga",
+                      artist: " Artist name : Asake"),
                 ],
               ),
             )));
